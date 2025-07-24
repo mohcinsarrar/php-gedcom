5cec273075ce82d418aedc3ac518011587695b11
mohcinsarrar <43006742+mohcinsarrar@users.noreply.github.com>
2025-07-24 20:55:53 +0100

    Update Indi.php

--- src/Parser/Indi.php
+++ src/Parser/Indi.php
@@ -17,7 +17,7 @@ namespace Gedcom\Parser;
 
 class Indi extends \Gedcom\Parser\Component
 {
-    public static function parse(\Gedcom\Parser $parser) : mixed
+    public static function parse(\Gedcom\Parser $parser)
     {
         $record = $parser->getCurrentLineRecord();
         $depth = (int) $record[0];
